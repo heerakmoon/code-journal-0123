13910b4c7a075edbecc31b42b9f2de5cbc29d745
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-13 23:09:12 +0000

    Added submit event listener. Prevents page from refreshing when form is submitted and stores form's input values into a new object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-var $photoUrl = document.querySelector('#url');
 var $img = document.querySelector('img');
+var $form = document.querySelector('form');
+var $photoUrl = document.querySelector('#url');
+// var $title = document.querySelector('#title');
+// var $notes = document.querySelector('#notes');
 
 $photoUrl.addEventListener('input', function (e) {
   $img.setAttribute('src', $photoUrl.value);
+});
+
+$form.addEventListener('submit', function (e) {
+  event.preventDefault();
+  // var formObj = {
+  //   title: $title.value,
+  //   photoUrl: $photoUrl.value,
+  //   notes: $notes.value
+  // };
+  // console.log('formObj:', formObj);
 });
