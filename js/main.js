01caa473ba63c0a35af990f2b85385f1491fcc2f
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-13 23:54:09 +0000

    Anonynous function increments nextEntryId property so new entries will receive a different id. Adds new obj to beginning of entries array of data model

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var $img = document.querySelector('img');
 var $form = document.querySelector('form');
 var $photoUrl = document.querySelector('#url');
-// var $title = document.querySelector('#title');
-// var $notes = document.querySelector('#notes');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
 
 $photoUrl.addEventListener('input', function (e) {
   $img.setAttribute('src', $photoUrl.value);
@@ -10,10 +10,12 @@ $photoUrl.addEventListener('input', function (e) {
 
 $form.addEventListener('submit', function (e) {
   event.preventDefault();
-  // var formObj = {
-  //   title: $title.value,
-  //   photoUrl: $photoUrl.value,
-  //   notes: $notes.value
-  // };
-  // console.log('formObj:', formObj);
+  var formObj = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formObj);
 });
