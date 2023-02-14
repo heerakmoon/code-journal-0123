1782d560a905ad070f3839d27d31cc66dc4f3c17
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-14 00:19:30 +0000

    Added a beforeunload event listener to window obj that serializes the data model and stores it in localstorage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,8 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (e) {
+  var dataModelJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal-local-storage', dataModelJSON);
+});
