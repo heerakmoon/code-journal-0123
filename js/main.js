7a93abc14e0142199943b15c80af437385b8970e
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-14 19:55:22 +0000

    Created a domcontentloaded event listener on document that loops through data.entries array,  generates a dom tree for each entry and appends it to the ul

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var $form = document.querySelector('form');
 var $photoUrl = document.querySelector('#url');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
-// var $ul = document.querySelector('ul');
+var $ul = document.querySelector('ul');
 
 $photoUrl.addEventListener('input', function (e) {
   $img.setAttribute('src', $photoUrl.value);
@@ -23,32 +23,39 @@ $form.addEventListener('submit', function (e) {
   $form.reset();
 });
 
-// function renderEntry(entry) {
-//   var $li = document.createElement('li');
+function renderEntry(entry) {
+  var $li = document.createElement('li');
 
-//   var $rowDiv = document.createElement('div');
-//   $rowDiv.className = 'row';
-//   $li.appendChild($rowDiv);
+  var $rowDiv = document.createElement('div');
+  $rowDiv.className = 'row';
+  $li.appendChild($rowDiv);
 
-//   var $leftHalfDiv = document.createElement('div');
-//   $leftHalfDiv.className = 'column-half padding-r-12';
-//   $rowDiv.appendChild($leftHalfDiv);
+  var $leftHalfDiv = document.createElement('div');
+  $leftHalfDiv.className = 'column-half padding-r-12';
+  $rowDiv.appendChild($leftHalfDiv);
 
-//   var $liImg = document.createElement('img');
-//   $liImg.setAttribute('src', entry.photoUrl);
-//   $leftHalfDiv.appendChild($liImg);
+  var $liImg = document.createElement('img');
+  $liImg.setAttribute('src', entry.photoUrl);
+  $leftHalfDiv.appendChild($liImg);
 
-//   var $rightHalfDiv = document.createElement('div');
-//   $rightHalfDiv.className = 'column-half padding-l-12';
-//   $rowDiv.appendChild($rightHalfDiv);
+  var $rightHalfDiv = document.createElement('div');
+  $rightHalfDiv.className = 'column-half padding-l-12';
+  $rowDiv.appendChild($rightHalfDiv);
 
-//   var $liTitle = document.createElement('h2');
-//   $liTitle.textContent = entry.title;
-//   $rightHalfDiv.appendChild($liTitle);
+  var $liTitle = document.createElement('h2');
+  $liTitle.textContent = entry.title;
+  $rightHalfDiv.appendChild($liTitle);
 
-//   var $liNotes = document.createElement('p');
-//   $liNotes.textContent = entry.notes;
-//   $rightHalfDiv.appendChild($liNotes);
+  var $liNotes = document.createElement('p');
+  $liNotes.textContent = entry.notes;
+  $rightHalfDiv.appendChild($liNotes);
 
-//   return $li;
-// }
+  return $li;
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var itemInEntries = renderEntry(data.entries[i]);
+    $ul.appendChild(itemInEntries);
+  }
+});
