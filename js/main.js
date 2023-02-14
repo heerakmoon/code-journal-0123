2ba39aca45368b2bb1018e61d212c48472e13a9c
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-14 19:52:15 +0000

    Defined a function named renderEntry with a single parameter, entry. Entry, represents a single object from data.entries

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $form = document.querySelector('form');
 var $photoUrl = document.querySelector('#url');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
+// var $ul = document.querySelector('ul');
 
 $photoUrl.addEventListener('input', function (e) {
   $img.setAttribute('src', $photoUrl.value);
@@ -21,3 +22,33 @@ $form.addEventListener('submit', function (e) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+// function renderEntry(entry) {
+//   var $li = document.createElement('li');
+
+//   var $rowDiv = document.createElement('div');
+//   $rowDiv.className = 'row';
+//   $li.appendChild($rowDiv);
+
+//   var $leftHalfDiv = document.createElement('div');
+//   $leftHalfDiv.className = 'column-half padding-r-12';
+//   $rowDiv.appendChild($leftHalfDiv);
+
+//   var $liImg = document.createElement('img');
+//   $liImg.setAttribute('src', entry.photoUrl);
+//   $leftHalfDiv.appendChild($liImg);
+
+//   var $rightHalfDiv = document.createElement('div');
+//   $rightHalfDiv.className = 'column-half padding-l-12';
+//   $rowDiv.appendChild($rightHalfDiv);
+
+//   var $liTitle = document.createElement('h2');
+//   $liTitle.textContent = entry.title;
+//   $rightHalfDiv.appendChild($liTitle);
+
+//   var $liNotes = document.createElement('p');
+//   $liNotes.textContent = entry.notes;
+//   $rightHalfDiv.appendChild($liNotes);
+
+//   return $li;
+// }
